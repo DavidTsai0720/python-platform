a655b8cfa5e65a5319cfe606428c0e8ad404a3ff
SylvesterTsai <djuni0720@gmail.com>
2021-02-28 19:19:39 +0800

    rm setting

--- setting.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    filename="mylog.log",
-    level=logging.INFO,
-    datefmt="%Y-%m-%d %H:%M:%S",
-    filemode="w",
-    format="%(levelname)s %(asctime)s: %(message)s",
-)
