8282a561cc368740cf7e76d7d081a6a9503c15d6
SylvesterTsai <djuni0720@gmail.com>
2021-03-09 20:35:00 +0800

    change log mode

--- parse/setting.py
+++ parse/setting.py
@@ -4,6 +4,6 @@ logging.basicConfig(
     filename="mylog.log",
     level=logging.INFO,
     datefmt="%Y-%m-%d %H:%M:%S",
-    filemode="w",
+    filemode="a",
     format="%(levelname)s %(asctime)s: %(message)s at %(pathname)s %(lineno)d",
 )
